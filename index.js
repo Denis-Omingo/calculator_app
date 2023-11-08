7e8da26882cd0a94d9bb9e93b08c90cde31b16a9
Denis-Omingo <94627798+Denis-Omingo@users.noreply.github.com>
2023-11-08 10:14:47 +0300

    Add files via upload
    First Commit

---
+++ index.js
@@ -0,0 +1,126 @@
+const keys=document.querySelectorAll('.key');
+const display_input=document.querySelector('.display .input');
+const display_output=document.querySelector('.display .output');
+const cursor=document.querySelector(' .cursor');
+
+let input = "";
+
+for (let key of keys) {
+    const value=key.dataset.key;
+
+    key.addEventListener('click', ()=>{
+        
+        cursor.style.display = 'none';
+
+        if (value=="clear"){
+            input="";
+            display_input.innerHTML="";
+            display_output.innerHTML="";
+            cursor.style.display = 'inline-block';
+        }else if(value=="backspace"){
+            input=input.slice(0,-1);
+            display_input.innerHTML=CleanInput(input);
+        }else if (value== "="){
+            let output=eval(PrepareInput(input));
+
+            display_output.innerHTML=CleanOutput(output);
+        }else if (value=="brackets"){
+            if(input.indexOf("(")==-1 ||
+             input.indexOf("(") !=-1 &&
+              input.indexOf(")") && 
+              input.lastIndexOf("(")<input.lastIndexOf(")")){
+                input+="(";
+            }else if(
+                input.indexOf("(") !=-1 &&
+                input.indexOf(")")==-1 ||
+                input.indexOf("(")!=-1 &&
+                input.indexOf(")")!=-1 &&
+                input.lastIndexOf("(")> input.lastIndexOf(")")
+            ){
+                input+=")";
+            }
+            display_input.innerHTML=CleanInput(input);
+        }else {
+            if (ValidateInput(value)){
+                input+=value;
+                display_input.innerHTML=CleanInput(input);
+            }
+        }
+        
+        
+    })
+}
+
+//Clean Up Code
+function CleanInput(input) {
+    let input_array=input.split("");
+    let input_array_length=input_array.lenth;
+
+    for (let i=0; i<input_array_length; i++){
+        if(input_array[i]=="*"){
+            input_array[i]=`<span class="operator">x</span>`;
+        } else if (input_array[i]=="/"){
+            input_array[i]=`<span class="operator">/</span>`;
+        } else if (input_array[i]=="+"){
+            input_array[i]=`<span class="operator">+</span>`;
+        } else if (input_array[i]=="-"){
+            input_array[i]=`<span class="operator">-</span>`;
+        } else if (input_array[i]=="("){
+            input_array[i]=`<span class="operator">(</span>`;
+        } else if (input_array[i]==")"){
+            input_array[i]=`<span class="operator">)</span>`;
+        } else if (input_array[i]=="%"){
+            input_array[i]=`<span class="operator">%</span>`;
+        }
+    }
+    return input_array.join("");
+}
+
+function CleanOutput(output){
+    let output_string=output.toString();
+    let decimal=output_string.split(".")[1];
+    output_string= output_string.split(".")[0];
+
+    let output_array=output_string.split("");
+
+    if (output_array.length > 3) {
+        for (let i = output_array.length - 3; i > 0; i -= 3) {
+            output_array.splice(i, 0, ",");
+        }
+    }
+    
+    if (decimal) {
+        output_array.push(".");
+        output_array.push(decimal);
+    }
+
+    return output_array.join("");
+}
+
+function ValidateInput (value) {
+    let last_input=input.slice(-1);
+    let operators =["+","-","*","/"];
+
+    if (value=="." && last_input=="."){
+        return false;
+    }
+    if (operators.includes(value)){
+        if (operators.includes(last_input)){
+            return false;
+        }else {
+            return true;
+        }
+    }
+    return true;
+}
+
+function PrepareInput(input) {
+    let input_array=input.split("");
+
+    for (let i=0; i<input_array.length; i++){
+        if (input_array[i]=="%"){
+            input_array[i]="/100";
+        }
+    }
+    return input_array.join("");
+}
